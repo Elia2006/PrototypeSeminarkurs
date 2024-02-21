6dc6713877a8cfe9b8f7eafdcbb8364eab903d3f
justhlt <91086497+justhlt@users.noreply.github.com>
2024-02-21 21:12:29 +0100

    Teleporter Menü (unreal (engine))

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour
+{
+    public GameObject Canvas;
+    public bool mapOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            Canvas.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            Canvas.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+
+}
