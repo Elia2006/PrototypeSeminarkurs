f48e354deec4c6731f57d649d13528c667664664
Elia2006 <145025238+Elia2006@users.noreply.github.com>
2024-10-30 19:02:50 +0100

    asd

---
+++ Assets/Scripts/Player/Filmgrain.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Filmgrain : MonoBehaviour
+{
+    [SerializeField] Volume volume;
+    [SerializeField] Collider shield;
+    private FilmGrain filmGrain;
+    private float intensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!shield.bounds.Contains(transform.position))
+        {
+            volume.profile.TryGet(out filmGrain);
+            {
+                filmGrain.intensity.value = intensity;
+            }
+            intensity += Time.deltaTime * 0.3f;
+        }
+        else if(intensity > 0)
+        {
+            volume.profile.TryGet(out filmGrain);
+            {
+                filmGrain.intensity.value = intensity;
+            }
+            intensity -= Time.deltaTime * 0.3f;
+        }
+
+    }
+}
