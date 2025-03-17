acde6119ba8c68cde4ee2e8b01a3c561817a695d
Elia2006 <145025238+Elia2006@users.noreply.github.com>
2025-03-17 20:20:08 +0100

    Loader

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -6,10 +6,11 @@ public class Loader : MonoBehaviour
 {
     [SerializeField] Transform Player;
     private Enemy[] enemies;
+    private GameObject[] items;
     // Start is called before the first frame update
     void Start()
     {
-        enemies = GameObject.FindObjectsOfType<Enemy>();
+
 
         StartCoroutine(CheckIfLoaded());
     }
@@ -19,14 +20,27 @@ public class Loader : MonoBehaviour
     {
         while (true)
         {
+            enemies = GameObject.FindObjectsOfType<Enemy>();
+            items = GameObject.FindGameObjectsWithTag("Item");
             foreach(Enemy enemy in enemies)
             {
+                
                 if(Vector3.Distance(Player.position, enemy.transform.position) < 100)
                 {
                     enemy.gameObject.SetActive(true);
                 }else
                 {
                     enemy.gameObject.SetActive(false);
+                }
+            }
+            foreach(GameObject item in items)
+            {
+                if(Vector3.Distance(Player.position, item.transform.position) < 100)
+                {
+                    item.SetActive(true);
+                }else
+                {
+                    item.SetActive(false);
                 }
             }
 
